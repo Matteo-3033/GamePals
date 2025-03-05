3508400be5db8caac6ec93d98f49afa3ccce4a04
Filippo Corti <filippo.corti1@studenti.unimi.it>
2025-03-05 11:28:38 +0100

    fixed parser

--- utils/arg_parser.py
+++ utils/arg_parser.py
@@ -2,7 +2,6 @@ import argparse
 from typing import Any
 
 
-
 class ArgParser:
 
     def __init__(self):
@@ -10,12 +9,7 @@ class ArgParser:
             prog="Copilot",
             description="Copilot is a tool for emulating a video game controller, allowing gameplay using inputs from either a physical controller or an AI agent.",
         )
-        self._add_arguments(parser)
 
-        self.args = parser.parse_args()
-
-    def _add_arguments(self, parser: argparse.ArgumentParser) -> None:
-        """ Adds Arguments to the parser """
         parser.add_argument(
             "-c",
             "--config",
@@ -23,6 +17,14 @@ class ArgParser:
             help="Controller configuration file. An example configuration file can be found in the main repository of the project.",
             required=True,
         )
+
+        self._add_arguments(parser)
+
+        self.args = parser.parse_args()
+
+    def _add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        """ Adds Arguments to the parser """
+        pass
 
     def get_config(self) -> 'Config':
         from copilot.utils import Config
